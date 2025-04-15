dcd8e34182b6e0b3c6d0b0367e8321b71624c609
Sline-X <kipa.maksim99@gmail.com>
2025-04-16 00:44:14 +0300

    update tests

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -3,19 +3,44 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+//use Symfony\Component\Panther\PantherTestCase;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ConferenceControllerTest extends WebTestCase
+//class ConferenceControllerTest extends PantherTestCase
 {
     public function testSomething(): void
     {
         $client = static::createClient();
+        //$client = static::createPantherClient(['external_base_uri' => rtrim($_SERVER['SYMFONY_PROJECT_DEFAULT_ROUTE_URL'], '/')]);
         $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
     }
 
-    /**
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conference/amsterdam-2019');
+        $client->submitForm('Submit', [
+            'comment[author]' => 'Fabien',
+            'comment[text]' => 'Some feedback from an automated functional test',
+            'comment[email]' => $email = 'me@automat.ed',
+            'comment[photo]' => dirname(__DIR__, 2).'/public/images/under-construction.gif',
+        ]);
+        $this->assertResponseRedirects();
+
+        //simulate comment validation
+        $comment = self::getContainer()->get(CommentRepository::class)->findOneByEmail($email);
+        $comment->setState('published');
+        self::getContainer()->get(EntityManagerInterface::class)->flush();
+
+        $client->followRedirect();
+        $this->assertSelectorExists('div:contains("There are 2 comments")');
+    }
+
     public function testConferencePage()
     {
         $client = static::createClient();
@@ -29,20 +54,5 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
         $this->assertSelectorExists('div:contains("There are 1 comments")');
-    }*/
-    
-    public function testCommentSubmission()
-    {
-        $client = static::createClient();
-        $client->request('GET', '/conference/amsterdam-2019');
-        $client->submitForm('Submit', [
-            'comment[author]' => 'Fabien',
-            'comment[text]' => 'Some feedback from an automated functional test',
-            'comment[email]' => 'me@automat.ed',
-            'comment[photo]' => dirname(__DIR__, 2).'/public/images/under-construction.gif',
-        ]);
-        $this->assertResponseRedirects();
-        $client->followRedirect();
-        $this->assertSelectorExists('div:contains("There are 2 comments")');
     }
 }
