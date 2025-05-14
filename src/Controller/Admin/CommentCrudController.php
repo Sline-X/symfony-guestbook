6186d8c5e78b93c5f7091ac5fa78f56044bac342
Maksim Kipa <m.kipa@webpractik.ru>
2025-05-14 13:40:28 +0300

    chore(cs-fixer): правки кода фиксером

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -9,9 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
@@ -21,7 +19,7 @@ class CommentCrudController extends AbstractCrudController
     {
         return Comment::class;
     }
-    
+
     public function configureCrud(Crud $crud): Crud
     {
         return $crud
@@ -29,17 +27,16 @@ class CommentCrudController extends AbstractCrudController
             ->setEntityLabelInPlural('Conference comments')
             ->setSearchFields(['author', 'text', 'email'])
             ->setDefaultSort(['createdAt' => 'DESC'])
-            ;
+        ;
     }
-    
+
     public function configureFilters(Filters $filters): Filters
     {
         return $filters
             ->add(EntityFilter::new('conference'))
-            ;
+        ;
     }
 
-    
     public function configureFields(string $pageName): iterable
     {
         yield AssociationField::new('conference');
@@ -54,7 +51,7 @@ class CommentCrudController extends AbstractCrudController
             ->onlyOnIndex()
         ;
         yield TextField::new('state');
-        
+
         $createdAt = DateTimeField::new('createdAt')->setFormTypeOptions([
             'years' => range(date('Y'), date('Y') + 5),
             'widget' => 'single_text',
@@ -63,5 +60,4 @@ class CommentCrudController extends AbstractCrudController
             yield $createdAt->setFormTypeOption('disabled', true);
         }
     }
-    
 }
--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,7 +8,6 @@ use App\Form\CommentType;
 use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
-use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
@@ -24,79 +23,78 @@ class ConferenceController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
-        private MessageBusInterface $bus
+        private MessageBusInterface $bus,
     ) {
     }
-    
+
     #[Route('/')]
     public function indexNoLocale(): Response
     {
         return $this->redirectToRoute('homepage', ['_locale' => 'en']);
     }
-    
-    //#[Route('/conference', name: 'app_conference')]
+
+    // #[Route('/conference', name: 'app_conference')]
     #[Route('/{_locale<%app.supported_locales%>}/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {
-        /**
+        /*
          * return $this->render('conference/index.html.twig', [
          * 'controller_name' => 'ConferenceController',
          * ]);
          */
-        
+
         return $this->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
-        // ])->setSharedMaxAge(3600);
+            // ])->setSharedMaxAge(3600);
         ]);
     }
-    
+
     #[Route('/{_locale<%app.supported_locales%>}/conference/{slug}', name: 'conference')]
     public function show(
         Request $request,
         Conference $conference,
         CommentRepository $commentRepository,
         NotifierInterface $notifier,
-        #[Autowire('%photo_dir%')] string $photoDir
+        #[Autowire('%photo_dir%')] string $photoDir,
         // ConferenceRepository $conferenceRepository
-    ): Response
-    {
+    ): Response {
         $comment = new Comment();
         $form = $this->createForm(CommentType::class, $comment);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $comment->setConference($conference);
             if ($photo = $form['photo']->getData()) {
-                $filename = bin2hex(random_bytes(6)) .'.'. $photo->getExtension();
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->getExtension();
                 $photo->move($photoDir, $filename);
                 $comment->setPhotoFilename($filename);
             }
-            
+
             $this->entityManager->persist($comment);
             $this->entityManager->flush();
-            
+
             $context = [
                 'user_ip' => $request->getClientIp(),
                 'user_agent' => $request->headers->get('User-Agent'),
                 'referrer' => $request->headers->get('referer'),
                 'permalink' => $request->getUri(),
             ];
-            
+
             $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
             // $reviewUrl = $this->generateUrl('review_comment', ['id' => $comment->getId()], UrlGenerator::ABSOLUTE_URL);
             // $this->bus->dispatch(new CommentMessage($comment->getId(), $reviewUrl, $context));
-            
+
             $notifier->send(new Notification('Thank you for the feedback; your comment will be posted after moderation.', ['browser']));
-            
+
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
         }
-        
+
         if ($form->isSubmitted()) {
             $notifier->send(new Notification('Can you check your submission? There are some problems with it.', ['browser']));
         }
-        
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
-        
+
         return $this->render('conference/show.html.twig', [
             // 'conferences' => $conferenceRepository->findAll(),
             'conference' => $conference,
@@ -104,9 +102,9 @@ class ConferenceController extends AbstractController
             'previous' => $offset - CommentRepository::COMMENTS_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::COMMENTS_PER_PAGE),
             'comment_form' => $form,
-    ]);
+        ]);
     }
-    
+
     #[Route('/conference_header', name: 'conference_header')]
     public function conferenceHeader(ConferenceRepository $conferenceRepository): Response
     {
